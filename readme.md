fd0daca3774a876ad6d52923f220de204d7f279e
harekrishnarai <786hkr@gmail.com>
2024-03-30 21:55:03 +0530

    CVE-2024-3094 Checker :seedling:

---
+++ readme.md
@@ -0,0 +1,28 @@
+# xz-utils-vuln-checker
+
+This is a bash script designed to check for a specific vulnerability (CVE-2024-3094) in xz-utils.
+
+## Getting Started
+
+These instructions will get you a copy of the script up and running on your local machine for vulnerability checking purposes.
+
+### Prerequisites
+
+You need to have a bash shell to run this script. This is typically available on most Unix-like operating systems, including Linux and Mac OS X.
+
+
+### Installing
+
+To use this script, you can simply download it and give it execute permissions:
+
+```bash
+git clone https://github.com/harekrishnarai/xz-utils-vuln-checker
+```
+```bash
+chmod +x xz-utils-vuln-checker.sh
+```
+```
+The available options are:
+-s, --summary       Display a short summary about CVE-2024-3094
+-h, --help: Display the help message
+```
---
+++ xz-utils-vuln-checker.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+echo "
+                              __     _     __        
+   _  __ ____         __  __  / /_   (_)   / /   _____
+  | |/_//_  / ______ / / / / / __/  / /   / /   / ___/
+ _>  <   / /_/_____// /_/ / / /_   / /   / /   (__  ) 
+/_/|_|  /___/       \__,_/  \__/  /_/   /_/   /____/  
+                                                      
+							Hare Krishna Rai (0xblurr3d)               
+ _   __  __  __   / /   ____          _____   / /_   ___   _____   / /__  ___    _____
+| | / / / / / /  / /   / __ \ ______ / ___/  / __ \ / _ \ / ___/  / //_/ / _ \  / ___/
+| |/ / / /_/ /  / /   / / / //_____// /__   / / / //  __// /__   / ,<   /  __/ / /    
+|___/  \__,_/  /_/   /_/ /_/        \___/  /_/ /_/ \___/ \___/  /_/|_|  \___/ /_/     
+(CVE-2024-3094)                                                                          
+"
+
+# Function to print text in red color
+print_red() {
+    printf "\033[1;31m$1\033[0m\n"
+}
+
+# Function to print text in green color
+print_green() {
+    printf "\033[1;32m$1\033[0m\n"
+}
+print_blue() {
+    printf "\033[1;34m$1\033[0m\n"
+}
+
+print_help() {
+    print_blue "Usage: ./xz_vuln_checker.sh [options]"
+    print_blue "Options:"
+    print_blue "  -h, --help          Display this help message"
+    print_blue "  -s, --summary       Display a short summary about CVE-2024-3094"
+    exit 0
+}
+# Print CVE-2024-3094 summary
+print_cve_summary() {
+    echo "Description:"
+    echo "Malicious code was discovered in the upstream tarballs of xz, starting with version 5.6.0. Through a series of complex obfuscations, the liblzma build process extracts a prebuilt object file from a disguised test file existing in the source code, which is then used to modify specific functions in the liblzma code. This results in a modified liblzma library that can be used by any software linked against this library, intercepting and modifying the data interaction with this library."
+    echo ""
+    echo "Resources:"
+    echo "1. Red Hat: https://access.redhat.com/security/cve/CVE-2024-3094"
+    echo "2. Ars Technica: https://arstechnica.com/security/2024/03/backdoor-found-in-widely-used-linux-utility-breaks-encrypted-ssh-connections/"
+    echo "3. AWS: https://aws.amazon.com/security/security-bulletins/AWS-2024-002/"
+    echo "12. Dark Reading: https://www.darkreading.com/vulnerabilities-threats/are-you-affected-by-the-backdoor-in-xz-utils"
+    echo "15. Tenable Blog: https://www.tenable.com/blog/frequently-asked-questions-cve-2024-3094-supply-chain-backdoor-in-xz-utils"
+    exit 0
+}
+if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+    print_help
+fi
+
+# Check for summary option
+if [[ "$1" == "-s" || "$1" == "--summary" ]]; then
+    print_cve_summary
+fi
+echo "[+] Initiating vulnerability check..."
+sleep 1
+echo "[+] Detecting package manager..."
+# Detect package manager
+if command -v apt-get &>/dev/null; then
+    PKG_MANAGER="apt-get"
+elif command -v yum &>/dev/null; then
+    PKG_MANAGER="yum"
+elif command -v zypper &>/dev/null; then
+    PKG_MANAGER="zypper"
+else
+    echo "Unsupported package manager. Exiting."
+    exit 1
+fi
+
+sleep 1
+echo "[+] Getting xz-utils version..."
+# Get xz-utils version
+case $PKG_MANAGER in
+    apt-get)
+        version=$(dpkg -l | grep "xz-utils" | awk '{print $3}')
+        ;;
+    yum)
+        version=$(rpm -qa | grep "xz" | awk '{print $1}')
+        ;;
+    zypper)
+        version=$(rpm -qa | grep "xz" | awk '{print $1}')
+        ;;
+esac
+
+sleep 1
+
+# Check if vulnerable version is installed
+echo "[+] Checking xz-utils version for vulnerabilities..."
+
+# Check if vulnerable version is installed
+if [[ "$version" == *"5.6.0"* || "$version" == *"5.6.1"* ]]; then
+    if [[ "$PKG_MANAGER" == "apt-get" && "$version" == *"5.6.1+really5.4.5-1"* ]]; then
+        print_green "You appear to be safe."
+    else
+        print_red "Vulnerable version of xz-utils found: $version"
+    fi
+else
+    print_green "You appear to be safe."
+fi
